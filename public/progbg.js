91bcd0f6d93e8532d516a67dd0e90757addb9258
Daniel-Mens <57283066+Daniel-Mens@users.noreply.github.com>
2021-05-19 17:46:06 +0200

    Added progressive background

---
+++ public/progbg.js
@@ -0,0 +1,36 @@
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+let steps = Math.round(Math.log(Math.max(screen.width, screen.height)) / Math.log(6));
+let seed;
+if (getCookie("bgseed") == "") {
+    seed = Math.random();
+    setCookie("bgseed", seed, 3 / 24 / 60);
+} else
+    seed = getCookie("bgseed");
+
+let prefix = "";
+for (var i = 0; i <= steps; i++) {
+    prefix += `<div class="progbg" style="background-image: url('https://picsum.photos/seed/${seed}/${Math.pow(7,i)}')">`;
+}
+document.body.innerHTML = prefix + document.body.innerHTML + "</div>".repeat(steps);
