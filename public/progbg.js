b37ae83a3f340e324e4e38874ebd8545f9416d60
tbhmens <57283066+tbhmens@users.noreply.github.com>
2021-06-19 14:11:52 +0200

    Fixed progbg, updated dependencies, minified the youtube logo

--- public/progbg.js
+++ public/progbg.js
@@ -4,21 +4,16 @@ function setWithExpiry() {
     return now;
 }
 
-function getBgSeed(key) {
-    const item = localStorage.getItem(key);
-    if (isNaN(item)) {
+function getBgSeed() {
+    const item = localStorage.getItem("bgseed");
+    if (isNaN(item) || Date.now() > parseInt(item))
         return setWithExpiry();
-    }
-    if (Date.now() > parseInt(item)) {
-        localStorage.removeItem(key);
-        return setWithExpiry();
-    }
     return item;
 }
 const steps = Math.round(Math.log(Math.max(screen.width, screen.height)) / Math.log(4));
 const seed = getBgSeed();
 let prefix = "";
 for (var i = 0; i <= steps; i++) {
-    prefix += `<div class="progbg" style="background-image: url('https://picsum.photos/seed/${seed}/${Math.pow(4,i)}')">`;
+    prefix += `<div class="progbg" style="background-image: url('https://picsum.photos/seed/${seed}/${Math.pow(4, i)}')">`;
 }
 document.body.innerHTML = prefix + document.body.innerHTML + "</div>".repeat(steps);
