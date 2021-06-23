cea270b633ff022d281604c7678ad793c02e6345
tbhmens <57283066+tbhmens@users.noreply.github.com>
2021-06-24 00:22:14 +0200

    Added automatic dark mode, along with a couple other design fixes.

--- public/progbg.js
+++ public/progbg.js
@@ -13,13 +13,10 @@ const size = Math.max(screen.width, screen.height);
 const steps = Math.log2(size) / 2;
 const seed = getBgSeed();
 let prefix = "";
-for (var i = 0; i < steps; i++) {
+for (var i = 0; i < steps; i++)
   prefix =
     `<div class="progbg" style="background-image: url('https://picsum.photos/seed/${seed}/${Math.round(
       size / Math.pow(4, i)
     )}')">` + prefix;
-}
-window.addEventListener("load", () => {
-  document.body.innerHTML =
-    prefix + document.body.innerHTML + "</div>".repeat(steps);
-});
+document.body.innerHTML =
+  prefix + document.body.innerHTML + "</div>".repeat(steps);
